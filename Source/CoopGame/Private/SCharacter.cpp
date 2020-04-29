45dcd2012c1899354d1150bc2e22cbd06a680d5e
NuclearGecko74 <39230191+NuclearGecko74@users.noreply.github.com>
2020-04-29 16:13:57 -0500

    Third Person Camera v1.0
    Configuramos la Cámara

--- Source/CoopGame/Private/SCharacter.cpp
+++ Source/CoopGame/Private/SCharacter.cpp
@@ -3,6 +3,7 @@
 
 #include "SCharacter.h"
 #include "Components/InputComponent.h"
+#include "Camera/CameraComponent.h"
 
 // Sets default values
 ASCharacter::ASCharacter()
@@ -10,6 +11,8 @@ ASCharacter::ASCharacter()
  	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	CameraComp = CreateDefaultSubobject<UCameraComponent>(TEXT("CameraComp"));
+	CameraComp->bUsePawnControlRotation = true;
 }
 
 // Called when the game starts or when spawned
