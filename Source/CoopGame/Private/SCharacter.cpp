01e823e4e233e006f60a289d3f850965fa94deb7
NuclearGecko74 <39230191+NuclearGecko74@users.noreply.github.com>
2020-04-29 15:42:14 -0500

    Add Movement Input
    Añadimos dos inputsAxis y Modificamos el ASCharacter, ahora puedes moverte con W, A, S ,D , obteniendo la funcion cada vez que presionamos la tecla

--- Source/CoopGame/Private/SCharacter.cpp
+++ Source/CoopGame/Private/SCharacter.cpp
@@ -2,6 +2,7 @@
 
 
 #include "SCharacter.h"
+#include "Components/InputComponent.h"
 
 // Sets default values
 ASCharacter::ASCharacter()
@@ -18,6 +19,16 @@ void ASCharacter::BeginPlay()
 	
 }
 
+void ASCharacter::MoveForward(float Value)
+{
+	AddMovementInput(GetActorForwardVector() * Value);
+}
+
+void ASCharacter::MoveRight(float Value)
+{
+	AddMovementInput(GetActorRightVector() * Value);
+}
+
 // Called every frame
 void ASCharacter::Tick(float DeltaTime)
 {
@@ -30,5 +41,7 @@ void ASCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponen
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
 
+	PlayerInputComponent->BindAxis("MoveForward", this, &ASCharacter::MoveForward);
+	PlayerInputComponent->BindAxis("MoveRight", this, &ASCharacter::MoveRight);
 }
 
